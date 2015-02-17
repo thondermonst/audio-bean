bde98b67810c7001e7b8a70ef8665b47a6cc8617
t.vermost <t.vermost@essent.be>
2015-02-17 16:03:27 +0100

    v0.2

---
+++ application/views/about.php
@@ -0,0 +1,7 @@
+<div class="message" style="display: none;">
+    
+</div>
+<div id="display">
+    <p>This is a small PHP CRUD application build by Tom Vermost using CodeIgniter, RedBeanPHP ORM and MySQL.<br /><br />
+    Interested? Grab the code from GitHub <?php print anchor_popup('https://github.com/thondermonst/audio-bean', 'here'); ?>!</p>
+</div>
